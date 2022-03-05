02f962495526b67cd910a330c6f34cd2ecf70e14
ANANTHU-R-NATH <ananthurnath52@gmail.com>
2022-03-05 15:37:18 +0530

    added missing comment in CategoryMaster

--- CategoryMaster.cs
+++ CategoryMaster.cs
@@ -20,6 +20,9 @@ namespace Quiz
         #endregion
 
         #region Helper Methods
+        /// <summary>
+        /// This method Populated the Grid View with data from CategoryMaster Table
+        /// </summary>
         private void Populate()
         {
             dgv_CategoryMaster.Rows.Clear();
@@ -34,6 +37,12 @@ namespace Quiz
             dgv_CategoryMaster.Rows.Add();
             PrepareRow(dgv_CategoryMaster.CurrentRow.Index);
         }
+        /// <summary>
+        /// This method Prepopulates Button text in a Row
+        /// </summary>
+        /// <param>
+        /// <c>index</c> The index of the row to be processed
+        /// </param>
         private void PrepareRow(int index)
         {
 
@@ -41,6 +50,15 @@ namespace Quiz
             dgv_CategoryMaster.Rows[index].Cells[3].Value = "Delete";
 
         }
+        /// <summary>
+        /// This method Diables or Enables Editing of a given Row
+        /// </summary>
+        /// <param>
+        /// <c>row</c> is the Index of Perform the Action on.
+        /// </param>
+        ///  /// <param>
+        /// <c>Disable</c> contains value for Disble/Enable.
+        /// </param>
         private void DisableEdit(int row, bool Disable)
         {
             if (!Disable && (row == dgv_CategoryMaster.Rows.Count - 1))
@@ -52,10 +70,20 @@ namespace Quiz
             dgv_CategoryMaster.Rows[row].Cells[1].Style.BackColor = Disable ? Color.White : Color.Tan;
 
         }
+        /// <summary>
+        /// This method Deletes a Row from the Data Grid View.
+        /// </summary>
+        /// <param>
+        /// <c>index</c> is index of the row to be deleted.
+        /// </param>
         private void DeleteRow(int index)
         {
             dgv_CategoryMaster.Rows.RemoveAt(index);
         }
+        /// <summary>
+        /// This method Validates the GridView on Sumbit.
+        /// </summary>
+        /// <param>
         private bool validateGridView()
         {
             foreach (DataGridViewRow row in dgv_CategoryMaster.Rows)
